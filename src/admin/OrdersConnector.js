89452552709ca9ae1f259299774004ed0bacc888
Phuc Tran <tranquangphuc@outlook.com>
2020-01-29 20:26:25 +0700

    ship order

--- src/admin/OrdersConnector.js
+++ src/admin/OrdersConnector.js
@@ -1,27 +1,36 @@
 import { graphql } from 'react-apollo';
+import { flowRight } from 'lodash';
 import { ordersSummaryQuery } from './clientQueries';
+import { shipOrder } from './clientMutations';
 import { OrdersTable } from './OrdersTable';
 const vars = { onlyShipped: false, page: 1, pageSize: 10, sort: 'id' };
-export const OrdersConnector = graphql(ordersSummaryQuery, {
-  options: props => ({ variables: vars }),
-  props: ({ data: { loading, orders, refetch } }) => ({
-    totalSize: loading ? 0 : orders.totalSize,
-    orders: loading ? [] : orders.orders,
-    pageMax: loading ? 0 : Math.ceil(orders.totalSize / vars.pageSize),
-    page: vars.page,
-    navigateToPage: page => {
-      vars.page = Number(page);
-      refetch(vars);
-    },
-    pageSize: vars.pageSize,
-    setPageSize: size => {
-      vars.pageSize = Number(size);
-      refetch(vars);
-    },
-    sortKey: vars.sort,
-    setSortKey: key => {
-      vars.sort = key;
-      refetch(vars);
-    }
+export const OrdersConnector = flowRight(
+  graphql(ordersSummaryQuery, {
+    options: props => ({ variables: vars }),
+    props: ({ data: { loading, orders, refetch } }) => ({
+      totalSize: loading ? 0 : orders.totalSize,
+      orders: loading ? [] : orders.orders,
+      pageMax: loading ? 0 : Math.ceil(orders.totalSize / vars.pageSize),
+      page: vars.page,
+      navigateToPage: page => {
+        vars.page = Number(page);
+        refetch(vars);
+      },
+      pageSize: vars.pageSize,
+      setPageSize: size => {
+        vars.pageSize = Number(size);
+        refetch(vars);
+      },
+      sortKey: vars.sort,
+      setSortKey: key => {
+        vars.sort = key;
+        refetch(vars);
+      }
+    })
+  }),
+  graphql(shipOrder, {
+    props: ({ mutate }) => ({
+      toggleShipped: (id, shipped) => mutate({ variables: { id, shipped } })
+    })
   })
-})(OrdersTable);
+)(OrdersTable);
